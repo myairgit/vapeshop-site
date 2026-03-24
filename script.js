71fe461d381efd83916cfbdcc3fa18c1fdb1e015
Vadim <myairgit@gmail.com>
2026-03-24 21:52:59 +0100

    1.2.5

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function enter(){
-document.getElementById("agePopup").style.display="none";
+const ageVerified = localStorage.getItem("ageVerified")
+
+if (ageVerified === "true") {
+  document.getElementById("agePopup").style.display = "none"
+} else {
+  document.getElementById("agePopup").style.display = "flex"
+}
+
+window.enter = () => {
+  localStorage.setItem("ageVerified", "true")
+  document.getElementById("agePopup").style.display = "none"
 }
